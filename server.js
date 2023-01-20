130b2467e0bcdfa56c7a12e571105cb904b7ad09
“Gigi <debortolg@gmail.com>
2023-01-20 15:14:54 -0800

    added to app.js file, removed old port info and kept sequelize sync

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+
+// import connections for sequelize 
 const sequelize = require('./config/connection');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,12 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// sync sequelize models to the database via connection file 
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Server now listening on ${PORT}!`));
 });
 
-// app.listen(PORT, () => {
-//   console.log(`App listening on port ${PORT}!`);
-// });
+
