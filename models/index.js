ab3c20d42d664f16f34f20178e65c0cff60f8952
“Gigi <debortolg@gmail.com>
2023-01-20 18:04:30 -0800

    working on routes

--- models/index.js
+++ models/index.js
@@ -4,14 +4,13 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
+// Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id'
 });
 
 // Categories have many Products
-
 Category.hasMany(Product, {
   foreignKey: 'category_id',
 })
