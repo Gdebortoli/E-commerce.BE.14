2aee3a98d4a19a781de05d3d46e85e79311baeff
“Gigi <debortolg@gmail.com>
2023-01-20 17:23:59 -0800

    was able to get the server to work

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,36 @@ require('dotenv').config();
 // import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 // create connection/route to our database
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
+
+let sequelize;
+
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+    sequelize = new Sequelize(
+        process.env.MYSQL_DB,
+        process.env.MYSQL_USER,
+        process.env.MYSQL_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306,
+        }
+    );
+}
+
+
+
+
+
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       user: 'root',
+//       password: 'password',
+//       port: 3306
+//     }
+//   );
 
 module.exports = sequelize;
